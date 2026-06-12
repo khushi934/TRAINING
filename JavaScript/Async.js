5dbfd186a265f3b323fc5847a18d428ee322485c
Your Name <khushichaudharybst0@gmail.com>
2026-06-12 14:46:07 +0530

    this is async and await

---
+++ JavaScript/Async.js
@@ -0,0 +1,17 @@
+function getData(dataId,getNextData){
+    setTimeout(()=> {
+        console.log("DataId:"+dataId)
+        if(getNextData){
+            getNextData()
+        }
+    },5000)
+
+}
+getData(1,()=>{
+    getData(2,()=>{
+        getData(3,()=>{
+            getData(4)
+        })
+    })
+
+})
---
+++ JavaScript/Function.js
@@ -0,0 +1,11 @@
+// Arrow function
+const sum =(a,b)=>{
+    return a+b
+}
+console.log(sum(2,3))
+// Function Declration
+function message(){
+    console.log("Hi,How are you ")
+}
+console.log(message())
+//F 
