1b161b6cfa03aaf6c4df70951734215b32c17f4c
Your Name <khushichaudharybst0@gmail.com>
2026-06-13 10:14:48 +0530

    this is about error

--- JavaScript/Async.js
+++ JavaScript/Async.js
@@ -1,17 +1,34 @@
-function getData(dataId,getNextData){
-    setTimeout(()=> {
-        console.log("DataId:"+dataId)
-        if(getNextData){
-            getNextData()
-        }
-    },5000)
+// function getData(dataId,getNextData){
+//     setTimeout(()=> {
+//         console.log("DataId:"+dataId)
+//         if(getNextData){
+//             getNextData()
+//         }
+//     },5000)
 
-}
-getData(1,()=>{
-    getData(2,()=>{
-        getData(3,()=>{
-            getData(4)
-        })
+// }
+// getData(1,()=>{
+//     getData(2,()=>{
+//         getData(3,()=>{
+//             getData(4)
+//         })
+//     })
+
+// })
+
+// console.log(1)
+// setTimeout(()=>{
+//     console.log(2)
+// },0)
+// console.log(3)
+
+//Promises
+function getData(dataId){
+    return new Promises((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(dataId)
+            reject("its not fulfilled")
+        },2000)
     })
-
-})
+}
+getData(1).then(()=>getData(2)).then(()=>getData(3)).catch(())
---
+++ JavaScript/Error.js
@@ -0,0 +1,14 @@
+console.log(1)
+try {
+    throw new Error("here lag is not defined");
+
+    console.lag(2)
+}
+catch(err){
+    console.log(err)
+}
+
+
+finally{
+    console.log(3)
+}
